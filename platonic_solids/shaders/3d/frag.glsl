239b571be082b699fdefbdb9d9082860c7c1c1e3
cgsgRP5 <135200022+cgsgRP5@users.noreply.github.com>
2023-06-09 19:21:14 +0300

    Add files via upload

--- platonic_solids/shaders/3d/frag.glsl
+++ platonic_solids/shaders/3d/frag.glsl
@@ -13,14 +13,23 @@ in vec3 DrawNormal;
 
 */
 
-vec3 getColor(vec3 C, int q) {
+vec3 getColor(vec3 C, int q)
+{
   vec3 color = DrawPos - C;
-  for(int i = 0; i < q; i++) {
+  for(int i = 0; i < q; i++)
+  {
   }
   return color;
 }
 
-void main(void) {
+void main(void)
+{
   float l = length(DrawPos.xyz);
+  vec4 c1, c2;
   out_color = vec4(vec3(0, 0, 1) * vec3(l * l * l * l * l), 1);
+  out_color = vec4(vec3(0, 0, .5) * vec3(abs(DrawPos.x) + abs(DrawPos.y) + abs(DrawPos.z)), 1);
+  c1 = vec4(vec3(1, 0, 0) * vec3(l * l * l * l * l), 1);
+  c2 = vec4(vec3(0, 0, 0.5) * vec3(abs(DrawPos.x) + abs(DrawPos.y) + abs(DrawPos.z)), 1);
+
+  out_color = mix(c1, c2, vec4(.5));
 }
